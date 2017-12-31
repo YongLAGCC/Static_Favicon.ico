4ccece81bd6fc10684f7812bd895fe55755fea74
YongLAGCC <1291001957.yz@gmail.com>
2017-12-30 21:14:06 -0500

    static favicon.ico

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express'); 
+const favicon = require("serve-favicon");
+const app = express(); 
+const staticAssets = __dirname + "/public";
+
+const faviconPath = __dirname + "/public/favicon.ico"; 
+
+app
+    .use(express.static(staticAssets))
+    .use(favicon(faviconPath))
+    .get('/api/profile',(req,res) =>{
+
+        var profile = {name: "will"}; 
+
+        res.send(profile); 
+
+    });
+
+    app.listen(3050); 
