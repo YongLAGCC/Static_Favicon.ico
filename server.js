421fbec2d0a1772ad67dd7a1ba6a77c03cdc05a5
YongLAGCC <1291001957.yz@gmail.com>
2017-12-30 23:10:50 -0500

    reduce templating mov

--- server.js
+++ server.js
@@ -8,12 +8,30 @@ const faviconPath = __dirname + "/public/favicon.ico";
 app
     .use(express.static(staticAssets))
     .use(favicon(faviconPath))
-    .get('/api/profile',(req,res) =>{
+    .get('/',(req,res) =>{
+        var title = "some other title"; 
 
-        var profile = {name: "will"}; 
+        var tweets= [
+            "my first tweet", 
+            "other tweet ", 
+            "yet another tweet", 
+        ];
+        var tweetsHTML = tweets.reduce((html, tweet) =>`${html}<li>${tweet}</li>`, "")
 
-        res.send(profile); 
+
+        res.send(`
+        <html>
+        <head>
+            <link rel="stylesheet" href="/css/main.css">
+        </head>
+        <body>
+            <h1>${title}</h1>
+            <ul>${tweetsHTML}</ul>
+            <script src='js/main.js'></script>
+        </body>
+        </html>
+        `)
 
     });
 
-    app.listen(3050); 
+    app.listen(3000); 
